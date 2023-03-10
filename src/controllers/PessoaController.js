e35888fcf6b7db7c762e934af8cebd6051ca5feb
Jean <jean-cabral@hotmail.com>
2023-03-10 10:33:29 -0300

    Aula 3 finalizada

--- src/controllers/PessoaController.js
+++ src/controllers/PessoaController.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 const db = require('../models');
 
 class PessoaController {
@@ -167,6 +168,36 @@ class PessoaController {
       const matriculas = await pessoa.getAulasMatriculadas();
 
       return res.status(200).json(matriculas);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+  static async pegaMatriculaPorTurma(req, res) {
+    const { turmaId } = req.params;
+    try {
+      const todasAsMatriculas = await db.Matriculas.findAndCountAll({
+        where: {
+          turma_id: Number(turmaId),
+          status: 'confirmado',
+        },
+      });
+      return res.status(200).json(todasAsMatriculas);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+  static async pegaTurmasLotadas(req, res) {
+    const lotacaoTurma = 2;
+    try {
+      const turmasLotadas = await db.Matriculas.findAndCountAll({
+        where: {
+          status: 'confirmado',
+        },
+        attributes: ['turma_id'],
+        group: ['turma_id'],
+        having: Sequelize.literal(`count(turma_id) >= ${lotacaoTurma}`),
+      });
+      return res.status(200).json(turmasLotadas.count);
     } catch (error) {
       return res.status(500).json(error.message);
     }
--- src/seeders/20230215191710-demo-turmas.js
+++ src/seeders/20230215191710-demo-turmas.js
@@ -5,34 +5,34 @@ module.exports = {
       [
         {
           data_inicio: '2020-02-01',
-          nivel_id: 4,
-          docente_id: 18,
+          nivel_id: 1,
+          docente_id: 1,
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
           data_inicio: '2020-02-01',
-          nivel_id: 5,
-          docente_id: 18,
+          nivel_id: 2,
+          docente_id: 1,
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
           data_inicio: '2020-02-01',
-          nivel_id: 6,
-          docente_id: 19,
+          nivel_id: 3,
+          docente_id: 2,
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
           data_inicio: '2020-07-01',
-          nivel_id: 6,
-          docente_id: 19,
+          nivel_id: 3,
+          docente_id: 2,
           createdAt: new Date(),
           updatedAt: new Date(),
         },
       ],
-      {}
+      {},
     );
   },
 
